99b450863f421b89b1fa36bf4d3db2126708c510
Hank <wowzeroz@gmail.com>
2022-11-23 11:00:20 +0800

    [Feature] 新增測試，沒有預算(noBudget)

---
+++ Budget/Budget/BudgetService.swift
@@ -0,0 +1,15 @@
+//
+//  BudgetService.swift
+//  Budget
+//
+//  Created by iOS App member on 2022/11/23.
+//
+
+import Foundation
+
+class BudgetService {
+    
+    func totalAmoumt() -> Int {
+        return 0
+    }
+}
